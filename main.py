01a5fbd14e1a9348ff1d6d4fa3f2e0c48431856f
ChessZra <johnezrasee100@gmail.com>
2023-05-23 02:59:38 -0500

    Messing with some async callbacks

--- main.py
+++
@@ -1,8 +0,0 @@
-import time
-
-
-
-text = "hello world"
-for char in text:
-    print(char, end="")
-    time.sleep(0.1)
--- typeWriter.js
+++ typeWriter.js
@@ -1,17 +1,63 @@
-let index = 0
-let idx = 0
-const text = ["div.ChessZra {", "front-end-developer = True;", "}"]
-
-function typeWriter() {
-    if (index < text[idx].length) {
-        document.getElementById("typeWriter" + idx).innerHTML += text[idx][index]
-        index += 1
-        setTimeout(typeWriter, 100)
+// Function: typeWriter (Asynchronous)
+// Description: Intends to write HTML code with the type writer effect
+// Parameters: 
+//      - idx (integer): index to write in text
+//      - elementId (string): the id of the HTML element to write in
+//      - i (integer): initialize it with 0, the function is recursive.
+//      - callback (function): function to call after the execution
+// Returns: None
+function typeWriter(idx, elementId, i, callback, ...params) {
+    if (i < text[idx].length) {
+        document.getElementById(elementId).innerHTML += text[idx][i];
+        i += 1;
+        setTimeout(typeWriter, 100, idx, elementId, i, callback, ...params);
     }
     else {
-        idx += 1
-        index = 0
-        setTimeout(typeWriter, 100)
+        // execute a callback function if the callback parameter is invoked
+        if (typeof callback === "function") {
+            callback(...params);
+        }
     }
 }
-typeWriter()
+
+// Function: backspace (Asynchronous)
+// Description: It removes the characters one by one with the type write effect
+// Parameters:
+//      - times (integer): the amount of backspaces you want
+//      - elementId (string): the id of the HTML element
+//      - callback (function): function to call after the execution
+// Returns: None
+function backspace(times, elementId, callback, ...params) {
+    if (times == 0) {
+         // execute a callback function if the callback parameter is invoked
+        if (typeof callback === 'function') {
+            callback(...params);
+        }
+        return;
+    }
+    let sentence = document.getElementById(elementId).innerHTML;
+    document.getElementById(elementId).innerHTML = sentence.substring(0, sentence.length - 1)
+    setTimeout(backspace, 100, times - 1, elementId, callback, ...params)
+}
+
+// Function: displayText 
+// Description: Driver Code
+// Parameters: None
+// Returns: None
+function displayText() {
+    iteration += 1
+    if (iteration == 0) {
+        typeWriter(0, "typeWriter0", 0, displayText);
+    } else if (iteration == 1) {
+        typeWriter(1, "typeWriter1", 0, displayText);
+    } else if (iteration == 2) {
+        backspace(6, "typeWriter1", typeWriter, 2, "typeWriter1", 0, displayText)
+    } else if (iteration == 3) {
+        typeWriter(3, "typeWriter2", 0);
+    }
+}
+
+let iteration = -1;
+const text = ["div.ChessZra {", "front-end-developer = False;", "True;", "}"];
+displayText();
+
